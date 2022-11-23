7310ff551080918ded5f70323b1b1d13f681aa52
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-23 08:54:32 +0545

    file changed

---
+++ list.R
@@ -0,0 +1,9 @@
+lis = list("Hello",1,c("mango","banana"))
+#print(lis)
+
+rollno = list(1,2,3)
+studentnames = list('rohan','tilak','gokul')
+
+combine = list(rollno, studentnames)
+names(combine) = c('rollno','studentnames')
+print(combine)
