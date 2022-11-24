93e485360e933aceea8ac1a58a4dc0ce447c51dd
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-24 09:28:18 +0545

    file changed

--- project1.R
+++ project1.R
@@ -4,3 +4,10 @@ a= matrix(nrow = 3, ncol = 3, byrow = TRUE)
 print(a)
 
 #wirte a R program to create a matrix taking a given cector of numbers as inupt. Display the metrix
+
+print("   ")
+vec = c(12:23)
+rowname = c('r1','r2','r3','r4')
+colname = c('c1','c2','c3')
+b = matrix(vec,byrow = TRUE,dimnames=list(rowname,colname),nrow = 4)
+print(b)
