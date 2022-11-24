71cb8acd31de870fac58fc5bf30c4a23c4538641
Neerajan-Dhakal <nirajandhakal634@gmail.com>
2022-11-24 08:44:18 +0545

    first project file

---
+++ project1.R
@@ -0,0 +1,6 @@
+# write a R program to create a blank metrix
+
+a= matrix(nrow = 3, ncol = 3, byrow = TRUE)
+print(a)
+
+#wirte a R program to create a matrix taking a given cector of numbers as inupt. Display the metrix
